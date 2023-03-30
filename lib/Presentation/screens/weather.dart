202609e9be873bc5c9f0afe3d6ed9043fcfb7e61
ashiq-firoz <ashiqfiroz08@gmail.com>
2023-03-30 21:30:31 +0530

    "Splash Screen added"

--- lib/Presentation/screens/weather.dart
+++ lib/Presentation/screens/weather.dart
@@ -42,23 +42,22 @@ class Weather1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 98, 97, 97),
-                Color.fromARGB(255, 92, 92, 92),
-                Color.fromARGB(255, 85, 85, 85),
-                Color.fromARGB(255, 66, 66, 66),
-              ],
-              stops: [
-              0.1,
-              0.25,
-              0.4,
-              0.9,
-            ],
-              transform: GradientRotation(pi/2),
-            )
-          ),
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+        colors: [
+          Color.fromARGB(255, 98, 97, 97),
+          Color.fromARGB(255, 92, 92, 92),
+          Color.fromARGB(255, 85, 85, 85),
+          Color.fromARGB(255, 66, 66, 66),
+        ],
+        stops: [
+          0.1,
+          0.25,
+          0.4,
+          0.9,
+        ],
+        transform: GradientRotation(pi / 2),
+      )),
       child: SafeArea(
         child: ListView(
           children: [
@@ -68,7 +67,7 @@ class Weather1 extends StatelessWidget {
             Row(
               mainAxisAlignment: MainAxisAlignment.start,
               children: const [
-                Counter(), //counter.dart
+                //Counter(), //counter.dart
                 SizedBox(
                   width: 60.0,
                 ),
